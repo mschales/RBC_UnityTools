e41bb637db9fdcbddfffb2b44e8d9f7906bda18d
mschales <mich.schales@gmail.com>
2018-11-12 11:20:36 +0200

    code reformat, some new features

--- Assets/Editor/RBC_UnityTools/RBCTools.cs
+++ Assets/Editor/RBC_UnityTools/RBCTools.cs
@@ -3,155 +3,138 @@ using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
 
-class RBCTools : EditorWindow
-{
+class RBCTools : EditorWindow {
     int c_select = 0;
-    string[] c_options = new string[]
-    { "Change name",
-      "Update tags",
-      "Material change",
-      "Add component",
-      "Remove component",
-    };
-
+    string[] c_options = new string[] { "Change name", "Update tags", "Material change", "Add component", "Remove component", "Update Static option", };
     int curSearchOption = 0;
-    string[] searchOption = new string[]
-    {"Exact name", "Contains string"};
-
+    string[] searchOption = new string[] { "Exact name", "Contains string" };
     string ObjecToSearch = "";
     string NewName = "";
-    bool IncludeChilds = false;
-    int SelectChild = 0;
+    string NewTag = "";
+    bool ExcludeParents = false;
+    string CustomSuffix = "";
+    bool NumericSuffix = false;
+    GameObject[] FoundGameObjects;
+    List<Transform> TempObjects = new List<Transform> ();
+    [MenuItem ("RBC Tools/Open Window")]
 
-    bool korjaus = true;
-    bool skorjaus = true;
-    public GameObject[] EsineHaku;
-    public List<Transform> TempObjects = new List<Transform>();
-
-    [MenuItem("RunByCoffee Tools/RBC Tools")]
-
-    public static void ShowWindow()
-    {
-        GetWindow(typeof(RBCTools));
+    public static void ShowWindow () {
+        GetWindow (typeof (RBCTools));
     }
 
-    void OnGUI()
-    {
-        GUILayout.Space(20);
-        c_select = EditorGUILayout.Popup("Select Function", c_select, c_options);
-        GUILayout.Space(10);
-        curSearchOption = EditorGUILayout.Popup("Search Options", curSearchOption, searchOption);
-        GUILayout.Space(10);
+    void OnGUI () {
+        GUILayout.Space (20);
+        c_select = EditorGUILayout.Popup ("Select Function", c_select, c_options);
+        GUILayout.Space (10);
+        curSearchOption = EditorGUILayout.Popup ("Search Options", curSearchOption, searchOption);
+        GUILayout.Space (10);
 
-        if (c_select == 0) 
-        {
-            GUILayout.Label("CHANGE GAMEOBJECT(S) NAME", EditorStyles.boldLabel);
-            GUILayout.Space(10);
-            ObjecToSearch = EditorGUILayout.TextField("Search by name", ObjecToSearch);
-            GUILayout.Space(5);
-            NewName = EditorGUILayout.TextField("Set new name", NewName);
-            GUILayout.Space(5);
-            IncludeChilds = EditorGUILayout.Toggle("Has childs ?", IncludeChilds);
-            GUILayout.Space(5);
-            SelectChild = EditorGUILayout.IntField("Child index", SelectChild);
-
-            GUILayout.Space(10);
-            GUI.contentColor = Color.white;
+        if (c_select == 0) {
+            GUILayout.Label ("CHANGE GAMEOBJECT(S) NAME", EditorStyles.boldLabel);
+            GUILayout.Space (10);
+            ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
+            GUILayout.Space (5);
+            NewName = EditorGUILayout.TextField ("Set new name", NewName);
+            GUILayout.Space (5);
+            ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
+            GUILayout.Space (5);
+            NumericSuffix = EditorGUILayout.Toggle ("Add Numeric Suffix", NumericSuffix);
+            GUILayout.Space (5);
+            CustomSuffix = EditorGUILayout.TextField ("Custom Suffix", CustomSuffix);
+            GUILayout.Space (10);
             GUI.backgroundColor = Color.grey;
         }
 
-        if (GUILayout.Button("START", GUILayout.Width(96), GUILayout.Height(36)))
-        {
-            if (c_select == 0) UpdateItemName(ObjecToSearch, NewName, IncludeChilds, SelectChild);
+        else if (c_select == 1) {
+            GUILayout.Label ("UPDATE GAMEOBJECT(S) TAG", EditorStyles.boldLabel);
+            GUILayout.Space (10);
+            ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
+            GUILayout.Space (5);
+            NewTag = EditorGUILayout.TagField ("Set new tag", NewTag);
+            GUILayout.Space (10);
+            GUI.backgroundColor = Color.grey;
+        }
+
+        if (GUILayout.Button ("START", GUILayout.Width (96), GUILayout.Height (36))) {
+            if (c_select == 0) UpdateItemName (ObjecToSearch, NewName, ExcludeParents, CustomSuffix);
+            if (c_select == 1) UpdateTags (ObjecToSearch, NewTag, ExcludeParents);
         }
     }
 
-    public void UpdateItemName(string itemSearch, string newName, bool includeChilds, int selectChild)
-    {
-        if (c_select == 0) 
-        {            
-            TempObjects.Clear(); // Let's clear temporary item list
-            EsineHaku = FindObjectsOfType<GameObject>();
+    public void UpdateItemName (string itemSearch, string newName, bool excludeParents, string customSuffix) {
+        TempObjects.Clear (); // Let's clear temporary item list
+        FoundGameObjects = FindObjectsOfType<GameObject> (); // Add scene gameobjects into list
 
-            if (includeChilds) 
-            {
-                if (curSearchOption == 0) 
-                {
-                    foreach (GameObject go in EsineHaku) 
-                    {
-                        if (go.transform.name == itemSearch && go.transform.childCount == 0) 
-                        {
-                            Debug.Log("Muffinssi: " + go.transform.name);
-                            // TempObjects.Add(go.transform);  // Add found items into temporary item list
-                        }
-                    }
-                }
-
-                else if (curSearchOption == 1) 
-                {
-                    if (itemSearch.Length < 4)
-                    {
-                        Debug.LogWarning("Item name must be at least 4 characters"); return;
-                    }
-
-                    foreach (GameObject go in EsineHaku) 
-                    {
-                        if (go.transform.name.Contains(itemSearch) && go.transform.childCount == 0) 
-                        {
-
-                            Debug.Log("Muffinssi: " + go.transform.name);
-                            // TempObjects.Add(go.transform);  // Add found items into temporary item list
-                        }
-                    }
-                }
-
-                foreach (Transform go in TempObjects) { // Loop items from temporary item list
-                    go.gameObject.transform.name = newName; // Change GameObject (defined as go) name with the new defined name
-                }
-
-                return;
+        if (curSearchOption == 0) {
+            if (itemSearch.Length < 2) {
+                Debug.LogError ("Item name must be at least 2 characters"); return;
             }
 
-            else if (!includeChilds) 
-            {
-                if (curSearchOption == 0)
-                {
-                    foreach (GameObject go in EsineHaku)
-                    {
-                        if (go.transform.name == itemSearch && go.transform.childCount == 0)
-                        {
-                            Debug.Log("Objekti: " + go.transform.name);
-                            // TempObjects.Add(go.transform);  // Add found items into temporary item list
-                        }
-                    }
-                }
-
-                if (curSearchOption == 1)
-                {
-                    if (itemSearch.Length < 4) 
-                    {
-                        Debug.LogWarning("Item name must be at least 4 characters"); return;
+            for (int x = 0; x < FoundGameObjects.Length; x++) {
+                if (FoundGameObjects[x].transform.name == itemSearch && FoundGameObjects[x].transform.childCount == 0) {
+                    if (excludeParents) {
+                        if (FoundGameObjects[x].transform.parent == null) TempObjects.Add (FoundGameObjects[x].transform); // Add found items (without parent) into temporary item list
                     }
 
-
-                    foreach (GameObject go in EsineHaku)
-                    {
-                        if (go.transform.name.Contains(itemSearch) && go.transform.childCount == 0)
-                        {
-                            Debug.Log("Objekti: " + go.transform.name);
-                            // TempObjects.Add(go.transform);  // Add found items into temporary item list
-                        }
+                    else {
+                        TempObjects.Add (FoundGameObjects[x].transform);  // Add found items into temporary item list
                     }
-                }
-
-                foreach (Transform go in TempObjects)
-                { // Loop items from temporary item list
-                    Debug.Log("Objekti: " + go.transform.name);
-                    // TempObjects.Add(go.transform);  // Add found items into temporary item list
                 }
             }
+        }
 
-            return;
+        else if (curSearchOption == 1) {
+            if (itemSearch.Length < 2) {
+                Debug.LogError ("Item name must be at least 2 characters"); return;
+            }
+
+            for (int x = 0; x < FoundGameObjects.Length; x++) {
+                if (FoundGameObjects[x].transform.name.Contains (itemSearch) && FoundGameObjects[x].transform.childCount == 0) {
+                    if (excludeParents) {
+                        if (FoundGameObjects[x].transform.parent == null) TempObjects.Add (FoundGameObjects[x].transform);
+                    }
+
+                    else {
+                        TempObjects.Add (FoundGameObjects[x].transform);  // Add found items into temporary item list
+                    }
+                }
+            }
+        }
+
+        for (int x = 0; x < TempObjects.Count; x++) { // Loop items from temporary item list
+            TempObjects[x].gameObject.transform.name = newName; // Change GameObject name with the new defined name
+            if (CustomSuffix.Length > 1) {
+                TempObjects[x].gameObject.transform.name = NewName + " " + customSuffix.Replace ("%n", x.ToString ());
+            }
+        }
+    }
+
+    public void UpdateTags (string itemSearch, string newTag, bool excludeParents) {
+        TempObjects.Clear (); // Let's clear temporary item list
+        FoundGameObjects = FindObjectsOfType<GameObject> (); // Add scene gameobjects into list
+
+        if (curSearchOption == 0) {
+            for (int x = 0; x < FoundGameObjects.Length; x++) {
+                if (FoundGameObjects[x].transform.name == itemSearch && FoundGameObjects[x].transform.childCount == 0) {
+                    TempObjects.Add (FoundGameObjects[x].transform);  // Add found items into temporary item list
+                }
+            }
+        }
+
+        else if (curSearchOption == 1) {
+            if (itemSearch.Length < 2) {
+                Debug.LogWarning ("Item name must be at least 2 characters"); return;
+            }
+
+            for (int x = 0; x < FoundGameObjects.Length; x++) {
+                if (FoundGameObjects[x].transform.name.Contains (itemSearch) && FoundGameObjects[x].transform.childCount == 0) {
+                    TempObjects.Add (FoundGameObjects[x].transform);  // Add found items into temporary item list
+                }
+            }
+        }
+
+        foreach (Transform go in TempObjects) { // Loop items from temporary item list
+            go.gameObject.transform.tag = newTag; // Change GameObject (defined as go) name with the new defined name
         }
     }
 }
