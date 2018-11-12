374cd3008a913bc36771d260945b4f515b6a4042
mschales <mich.schales@gmail.com>
2018-11-12 14:34:02 +0200

    Cleaning up code #3

--- Assets/Editor/RBC_UnityTools/RBCTools.cs
+++ Assets/Editor/RBC_UnityTools/RBCTools.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System.Collections;
 using System.Collections.Generic;
 
 class RBCTools : EditorWindow {
@@ -25,60 +24,41 @@ class RBCTools : EditorWindow {
     }
 
     void OnGUI () {
-        GUILayout.Space (20);
-        c_select = EditorGUILayout.Popup ("Select Function", c_select, c_options);
-        GUILayout.Space (10);
-        curSearchOption = EditorGUILayout.Popup ("Search Options", curSearchOption, searchOption);
+        GUILayout.Space (20); c_select = EditorGUILayout.Popup ("Select Function", c_select, c_options);
+        GUILayout.Space (10); curSearchOption = EditorGUILayout.Popup ("Search Options", curSearchOption, searchOption);
         GUILayout.Space (10);
 
         if (c_select == 0) {
             GUILayout.Label ("UPDATE GAMEOBJECT(S) NAME", EditorStyles.boldLabel);
-            GUILayout.Space (10);
-            ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
-            GUILayout.Space (5);
-            NewName = EditorGUILayout.TextField ("Set new name", NewName);
-            GUILayout.Space (5);
-            ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
-            GUILayout.Space (5);
-            CustomSuffix = EditorGUILayout.TextField ("Custom Suffix", CustomSuffix);
-            GUILayout.Space (10);
-            GUI.backgroundColor = Color.grey;
+            GUILayout.Space (10); ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
+            GUILayout.Space (5); NewName = EditorGUILayout.TextField ("Set new name", NewName);
+            GUILayout.Space (5); ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
+            GUILayout.Space (5); CustomSuffix = EditorGUILayout.TextField ("Custom Suffix", CustomSuffix);
+            GUILayout.Space (10); GUI.backgroundColor = Color.grey;
         }
 
         else if (c_select == 1) {
             GUILayout.Label ("UPDATE GAMEOBJECT(S) TAG", EditorStyles.boldLabel);
-            GUILayout.Space (10);
-            ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
-            GUILayout.Space (5);
-            ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
-            GUILayout.Space (5);
-            NewTag = EditorGUILayout.TagField ("Set new tag", NewTag);
-            GUILayout.Space (10);
-            GUI.backgroundColor = Color.grey;
+            GUILayout.Space (10); ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
+            GUILayout.Space (5); ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
+            GUILayout.Space (5); NewTag = EditorGUILayout.TagField ("Set new tag", NewTag);
+            GUILayout.Space (10); GUI.backgroundColor = Color.grey;
         }
 
         else if (c_select == 2) {
             GUILayout.Label ("UPDATE GAMEOBJECT(S) STATIC OPION", EditorStyles.boldLabel);
-            GUILayout.Space (10);
-            ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
-            GUILayout.Space (5);
-            ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
-            GUILayout.Space (5);
-            SetStatic = EditorGUILayout.Toggle ("Set object as static", SetStatic);
-            GUILayout.Space (10);
-            GUI.backgroundColor = Color.grey;
+            GUILayout.Space (10); ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
+            GUILayout.Space (5); ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
+            GUILayout.Space (5); SetStatic = EditorGUILayout.Toggle ("Set object as static", SetStatic);
+            GUILayout.Space (10); GUI.backgroundColor = Color.grey;
         }
-       
+
         else if (c_select == 3) {
             GUILayout.Label ("REMOVE COMPONENT FROM GAMEOBJECT(S)", EditorStyles.boldLabel);
-            GUILayout.Space (10);
-            ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
-            GUILayout.Space (5);
-            ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
-            GUILayout.Space (5);
-            curComponent = EditorGUILayout.Popup ("Select Component", curComponent, component_list);
-            GUILayout.Space (10);
-            GUI.backgroundColor = Color.grey;
+            GUILayout.Space (10); ObjecToSearch = EditorGUILayout.TextField ("Search by name", ObjecToSearch);
+            GUILayout.Space (5); ExcludeParents = EditorGUILayout.Toggle ("Exclude parents", ExcludeParents);
+            GUILayout.Space (5); curComponent = EditorGUILayout.Popup ("Select Component", curComponent, component_list);
+            GUILayout.Space (10); GUI.backgroundColor = Color.grey;
         }
 
         if (GUILayout.Button ("START", GUILayout.Width (96), GUILayout.Height (36))) {
@@ -89,10 +69,9 @@ class RBCTools : EditorWindow {
         }
     }
 
-    // COLLECT GAMEOBJECTS //
-    public void GetGameObjects(string itemSearch, bool excludeParents) {
+    // COLLECT GAMEOBJECTS
+    public void GetGameObjects (string itemSearch, bool excludeParents) {
         TempObjects.Clear (); // Let's clear temporary item list
-
         if (curSearchOption == 0) {
             if (itemSearch.Length < 2) {
                 Debug.LogError ("Item name must be at least 2 characters"); return;
@@ -103,7 +82,6 @@ class RBCTools : EditorWindow {
                     if (excludeParents) {
                         if (FoundGameObjects[x].transform.parent == null) TempObjects.Add (FoundGameObjects[x].transform); // Add found items (without parent) into temporary item list
                     }
-
                     else TempObjects.Add (FoundGameObjects[x].transform);  // Add found items into temporary item list
                 }
             }
@@ -119,17 +97,16 @@ class RBCTools : EditorWindow {
                     if (excludeParents) {
                         if (FoundGameObjects[x].transform.parent == null) TempObjects.Add (FoundGameObjects[x].transform);
                     }
-
                     else TempObjects.Add (FoundGameObjects[x].transform);  // Add found items into temporary item list
                 }
             }
         }
     }
 
+    // FUNCTIONS
     public void UpdateItemName (string itemSearch, string newName, bool excludeParents, string customSuffix) {
         GetGameObjects (itemSearch, excludeParents); // Get Scene GameObjects
         FoundGameObjects = FindObjectsOfType<GameObject> (); // Add scene gameobjects into list
-
         for (int x = 0; x < TempObjects.Count; x++) { // Loop items from temporary item list
             TempObjects[x].gameObject.transform.name = newName; // Change GameObject name with the new defined name
             if (CustomSuffix.Length > 1) {
@@ -141,7 +118,6 @@ class RBCTools : EditorWindow {
     public void UpdateTags (string itemSearch, string newTag, bool excludeParents) {
         GetGameObjects (itemSearch, excludeParents); // Get Scene GameObjects
         FoundGameObjects = FindObjectsOfType<GameObject> (); // Add scene gameobjects into list
-
         for (int x = 0; x < TempObjects.Count; x++) { // Loop items from temporary item list
             TempObjects[x].gameObject.transform.tag = newTag; // Change GameObject (defined as go) name with the new defined name
         }
@@ -150,7 +126,6 @@ class RBCTools : EditorWindow {
     public void UpdateStatic (string itemSearch, bool setStatic, bool excludeParents) {
         GetGameObjects (itemSearch, excludeParents); // Get Scene GameObjects
         FoundGameObjects = FindObjectsOfType<GameObject> (); // Add scene gameobjects into list
-
         for (int x = 0; x < TempObjects.Count; x++) { // Loop items from temporary item list
             TempObjects[x].gameObject.isStatic = setStatic; // Change GameObject (defined as go) name with the new defined name
         }
@@ -159,7 +134,6 @@ class RBCTools : EditorWindow {
     public void UpdateComponent (string itemSearch, int curComponent, bool excludeParents) {
         GetGameObjects (itemSearch, excludeParents); // Get Scene GameObjects
         FoundGameObjects = FindObjectsOfType<GameObject> (); // Add scene gameobjects into list
-
         for (int x = 0; x < TempObjects.Count; x++) { // Loop items from temporary item list
             DestroyImmediate (TempObjects[x].gameObject.GetComponent (component_list[curComponent])); // Change GameObject name with the new defined name
         }
